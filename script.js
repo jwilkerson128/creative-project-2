64c9b9a1df2f279e4ae18c7f60829a13bc53b44c
Joshua Wilkerson <joshua.wilkerson@gmail.com>
2020-02-19 20:18:02 -0700

    Began writing files

---
+++ script.js
@@ -0,0 +1,18 @@
+const APIKEY = "776d092e11e42585171c62d62634346bfe8cad42";
+
+document.getElementById("countrySubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("countryInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+
+  const url = "https://calendarific.com/api/v2/holidays" + "?api_key=" + APIKEY + "&country=" + value + "&year=2019";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+    });
+
+});
