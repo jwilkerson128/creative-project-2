78ff1436521adc3d89e913bbc85ae00191ded4ad
Joshua Wilkerson <joshua.wilkerson@gmail.com>
2020-02-21 12:58:54 -0700

    Got country selection working

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const APIKEY = "776d092e11e42585171c62d62634346bfe8cad42";
 
 document.getElementById("countrySubmit").addEventListener("click", function(event) {
   event.preventDefault();
-  const value = document.getElementById("countryInput").value;
+  const value = document.getElementById("countrySelect").value;
   if (value === "")
     return;
   console.log(value);
 
-  const url = "https://calendarific.com/api/v2/holidays" + "?api_key=" + APIKEY + "&country=" + value + "&year=2019";
+  const url = "https://calendarific.com/api/v2/holidays" + "?api_key=" + APIKEY + "&country=" + value + "&year=2020";
   fetch(url)
     .then(function(response) {
       return response.json();
